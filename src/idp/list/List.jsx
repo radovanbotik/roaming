63260edcf53805d07fc44acdaa3d2d966bb34f84
radovanbotik <radovanbotik@gmail.com>
2023-11-16 00:00:32 +0100

    removing container from List component

--- src/idp/list/List.jsx
+++ src/idp/list/List.jsx
@@ -5,7 +5,8 @@ const List = props => {
   const { setDestination, setOpen } = props;
   return (
     <section className=" text-gray-100 ">
-      <div className="container mx-auto flex flex-col items-center px-4 py-4 md:py-8 md:px-10 lg:px-8 xl:max-w-6xl ">
+      {/* add container */}
+      <div className=" mx-auto flex flex-col items-center px-4 py-4 md:py-8 md:px-10 lg:px-8 xl:max-w-6xl ">
         <Table setDestination={setDestination} setOpen={setOpen} />
       </div>
     </section>
